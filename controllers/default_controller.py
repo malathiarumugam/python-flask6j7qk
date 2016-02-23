63b0f1c39f76ca2765244c2767a360cc3833ff32
Malathi <malathim993@gmail.com>
2016-02-23 11:57:38 +0530

    python:2.7_created

---
+++ controllers/default_controller.py
@@ -0,0 +1,15 @@
+
+def demo_get() -> str:
+    return 'do some magic!'
+
+def demo_post() -> str:
+    return 'do some magic!'
+
+def demo_demo_id_get(demoId) -> str:
+    return 'do some magic!'
+
+def demo_demo_id_put(demoId) -> str:
+    return 'do some magic!'
+
+def demo_demo_id_delete(demoId) -> str:
+    return 'do some magic!'
